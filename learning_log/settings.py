f60b5f578b9721f2ba934700fa055f2ff5213898
Kenyiro Anazagasty <kenyicode@gmail.com>
2020-11-26 20:56:23 -0500

    Modified settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -153,5 +153,5 @@ if os.getcwd() == '/app':
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/' 
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, 'learning_log/static'),
     )
